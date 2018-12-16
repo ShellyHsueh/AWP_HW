6355309d6aeb16735d98b61283b0935e9f5a7dec
ShellyHsueh <hsueh821203@gmail.com>
2018-12-16 12:15:55 +0800

    heroku debuging: remove composer require exception from production mode

--- web/init.php
+++ web/init.php
@@ -2,17 +2,17 @@
 
 define('WEB_ROOT', __DIR__);
 
-// Require the google/apiclient library
-if (!file_exists($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php')) {
-  throw new \Exception('please run "composer require google/apiclient:~2.0 php-activerecord/php-activerecord" in "' . $_SERVER['DOCUMENT_ROOT'] .'"');
-}
-require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
-
 // Require all files we need
 $folders = array('config', 'infrastructure', 'domain');
 foreach ($folders as $folder) {
   require_once __DIR__ . '/' . $folder . '/init.php';
 }
+
+// Require the google/apiclient library when run on local
+if (ENV=='development' && !file_exists($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php')) {
+  throw new \Exception('please run "composer require google/apiclient:~2.0 php-activerecord/php-activerecord" in "' . $_SERVER['DOCUMENT_ROOT'] .'"');
+}
+require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 
 
 ?>
