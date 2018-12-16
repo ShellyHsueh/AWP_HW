d5400b81218fe6ec2aa1a5a99819067781065818
ShellyHsueh <hsueh821203@gmail.com>
2018-12-16 12:18:41 +0800

    heroku debuging

--- web/init.php
+++ web/init.php
@@ -8,10 +8,12 @@ foreach ($folders as $folder) {
   require_once __DIR__ . '/' . $folder . '/init.php';
 }
 
+var_dump('ENV: ' . ENV);
+var_dump('DOCUMENT_ROOT: ' . $_SERVER['DOCUMENT_ROOT']);
 // Require the google/apiclient library when run on local
-if (ENV=='development' && !file_exists($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php')) {
-  throw new \Exception('please run "composer require google/apiclient:~2.0 php-activerecord/php-activerecord" in "' . $_SERVER['DOCUMENT_ROOT'] .'"');
-}
+// if (ENV=='development' && !file_exists($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php')) {
+//   throw new \Exception('please run "composer require google/apiclient:~2.0 php-activerecord/php-activerecord" in "' . $_SERVER['DOCUMENT_ROOT'] .'"');
+// }
 require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 
 
